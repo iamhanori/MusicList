bd80f13ef4c0519d4769883b9c50598630875227
Hanul Kim <s2122@e-mirim.hs.kr>
2023-08-10 11:05:43 +0900

    데이터 수정

--- MusicList/Form1.Designer.cs
+++ MusicList/Form1.Designer.cs
@@ -63,6 +63,7 @@
             this.dataGridView1.Location = new System.Drawing.Point(12, 113);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
             this.dataGridView1.RowTemplate.Height = 23;
             this.dataGridView1.Size = new System.Drawing.Size(793, 530);
             this.dataGridView1.TabIndex = 0;
